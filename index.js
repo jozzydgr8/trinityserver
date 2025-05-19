a429bc7ce3c23282add222b815c2d03f61cb2368
jozzydgr8 <jozzydgreat1@gmail.com>
2025-05-19 15:45:44 -0700

    test

--- index.js
+++ index.js
@@ -12,18 +12,18 @@ app.use(express.json({ limit: "25mb" }));
 app.use(express.urlencoded({ extended: true, limit: "25mb" }));
 
 // Routes (update path as needed based on your project structure)
-const emailRoutes = require('../routes/emailConfig');
-const paymentRoutes = require('../routes/stripePayment');
-const retrieveStripe = require('../routes/retrievestripe');
+const emailRoutes = require('./routes/emailConfig');
+const paymentRoutes = require('./routes/stripePayment');
+const retrieveStripe = require('./routes/retrievestripe');
 
 app.use(emailRoutes);
 app.use(paymentRoutes);
 app.use(retrieveStripe);
 
-// app.listen(port, () => {
-//     console.log(`🚀 Server running on http://localhost:${port}`);
-// });
+app.listen(port, () => {
+    console.log(`🚀 Server running on http://localhost:${port}`);
+});
 
 // Export for Vercel
-module.exports = app;
-module.exports.handler = serverless(app);
+// module.exports = app;
+// module.exports.handler = serverless(app);
